23fddd51c8a34b2d61adf5fa5e3893efb2d67290
astridmarloeswigit <mulianque8829066@163.com>
2021-01-03 01:21:19 +0800

    init

---
+++ src/main/java/com/kingboy/KingboySpringbootJpaApplication.java
@@ -0,0 +1,12 @@
+package com.kingboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingboySpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingboySpringbootJpaApplication.class, args);
+	}
+}
