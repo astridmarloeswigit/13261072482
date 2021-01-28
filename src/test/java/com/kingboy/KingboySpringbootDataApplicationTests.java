c2f90906178e5f795974d5dfac4ffce4c3bab54e
astridmarloeswigit <mulianque8829066@163.com>
2021-01-29 01:45:49 +0800

    code formate

--- src/test/java/com/kingboy/KingboySpringbootDataApplicationTests.java
+++ src/test/java/com/kingboy/KingboySpringbootDataApplicationTests.java
@@ -3,28 +3,14 @@ package com.kingboy;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
-
-import javax.annotation.Resource;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KingboySpringbootDataApplicationTests {
 
-	@Test
-	public void loadContext(){
+    @Test
+    public void loadContext() {
 
-	}
+    }
 }
